8934fe268688a76cd33733875f4eca6a5de658ba
Pasquale Boemio <boemianrapsodi@gmail.com>
2013-10-23 09:33:09 +0200

    uhmmm bad travis, so bad travis

--- confs/run-chrome.sh
+++ confs/run-chrome.sh
@@ -1,1 +1,3 @@
-exec google-chrome --no-default-browser-check --no-first-run --disable-default-apps --use-fake-ui-for-media-stream "$@"
+rm -rf /tmp/chrome-prefs
+cp -rf confs/chrome-prefs /tmp
+exec google-chrome --user-data-dir=/tmp/chrome-prefs --no-default-browser-check --no-first-run --disable-default-apps --use-fake-ui-for-media-stream "$@"
