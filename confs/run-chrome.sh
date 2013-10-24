a4c34a27ce2195d11fdc0cc91454fb29dbd1139f
Pasquale Boemio <boemianrapsodi@gmail.com>
2013-10-24 20:01:53 +0200

    no sandboxmaybe works

--- confs/run-chrome.sh
+++ confs/run-chrome.sh
@@ -1,3 +1,3 @@
 rm -rf /tmp/chrome-prefs
 cp -rf confs/chrome-prefs /tmp
-exec google-chrome --user-data-dir=/tmp/chrome-prefs --no-default-browser-check --no-first-run --disable-default-apps --use-fake-ui-for-media-stream "$@"
+exec google-chrome --user-data-dir=/tmp/chrome-prefs --no-default-browser-check --no-first-run --disable-default-apps --use-fake-ui-for-media-stream --no-sandbox "$@"
